13958cb53c0083ef8d1f6d510589368bd15c9a4d
Rossana Baptista Queiroz <fellowsheep@gmail.com>
2023-06-05 21:53:51 -0300

    Create HelloFilters.py

---
+++ HelloOpenCV-Python/HelloFilters.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2 as cv
+
+img = cv.imread('baboon.png') #original
+
+imgGrayscale =  cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+imgBlurred5x5 = cv.blur(imgGrayscale,(5,5))
+imgBlurred15x15 = cv.blur(imgGrayscale,(15,15))
+
+#Gaussian Blur -- Coloque lado a lado com o Blur normal pra ver a diferença
+imgGaussianBlurred5x5 = cv.GaussianBlur(imgGrayscale,(5,5),0)
+imgGaussianBlurred15x15 = cv.GaussianBlur(imgGrayscale,(15,15),0)
+
+imgCanny1 = cv.Canny(imgGrayscale,50,100)
+imgCanny2 = cv.Canny(imgBlurred5x5,50,100)
+imgCanny3 = cv.Canny(imgBlurred15x15,50,100)
+
+kernel = np.ones((3,3),np.uint8)
+#Dilatação e Erosão -- teste com imagens diferentes!
+imgDilation = cv.dilate(imgCanny2,kernel,iterations = 1)
+imgErosion = cv.erode(imgDilation,kernel,iterations = 1)
+
+cv.imshow("Imagem Grayscale",imgGrayscale)
+cv.imshow("Imagem Blurred - 5x5",imgBlurred5x5)
+cv.imshow("Imagem Blurred - 15x15",imgBlurred15x15)
+
+cv.imshow("Deteccao Canny - Img Grayscale",imgCanny1)
+cv.imshow("Deteccao Canny - Blur 5x5",imgCanny2)
+cv.imshow("Deteccao Canny - Blur 15x15",imgCanny3)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
